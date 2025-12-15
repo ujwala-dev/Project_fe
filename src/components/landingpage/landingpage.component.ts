7ea8617068f020483e054a541b17d2d3393ca3c9
Varun Reddy <2464480@cognizant.com>
2025-12-15 11:48:03 +0530

    added components

---
+++ src/components/landingpage/landingpage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landingpage',
+  imports: [],
+  templateUrl: './landingpage.component.html',
+  styleUrl: './landingpage.component.css'
+})
+export class LandingpageComponent {
+
+}
