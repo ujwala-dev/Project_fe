e665148f27178ab8adc26e843f267fae8b5409e3
Agnihotri <2464483@cognizant.com>
2026-01-27 15:01:19 +0530

    Components segeration

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { TypewriterDirective } from "../landingpage/typewriter.directive";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [TypewriterDirective],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  constructor(private Router: Router) { }
+navigateTo(route: string): void {
+    this.Router.navigate([route]);
+  }
 }
